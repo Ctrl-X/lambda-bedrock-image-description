29d2cb36253a5850e56052d2407d10fe45c5416d
CtrlX <jcoupez@amazom.com>
2024-05-03 11:24:46 -0400

    add IAM roles to amplify

--- lib/amplify-stack.ts
+++ lib/amplify-stack.ts
@@ -1,6 +1,7 @@
 import { Duration, SecretValue, Stack, StackProps } from "aws-cdk-lib"
 import { App, GitHubSourceCodeProvider } from "@aws-cdk/aws-amplify-alpha"
 import { Construct } from "constructs"
+import * as iam from "aws-cdk-lib/aws-iam"
 
 export class AmplifyInfraStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,7 +10,15 @@ export class AmplifyInfraStack extends Stack {
         // You must create a secret to store your github Personnal access token
         //@see https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
 
+        // Create an IAM role for Amplify
+        const amplifyRole = new iam.Role(this, "AmplifyRole", {
+            assumedBy: new iam.ServicePrincipal("amplify.amazonaws.com"),
+        })
 
+        // Attach the necessary permissions to the role
+        amplifyRole.addManagedPolicy(
+            iam.ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess-Amplify"),
+        )
 
         // Creation of the Amplify Application
         const amplifyApp = new App(this, "productDescr-frontend ", {
@@ -20,6 +29,7 @@ export class AmplifyInfraStack extends Stack {
                     jsonField: "token",
                 }),
             }),
+            role: amplifyRole, // Associate the IAM role with the Amplify app
         })
         const masterBranch = amplifyApp.addBranch("main")
     }
