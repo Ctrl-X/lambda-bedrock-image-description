15fd03591e44960dc8a14b3f79253ded841f4863
CtrlX <jcoupez@amazom.com>
2024-05-03 12:44:21 -0400

    moved amplify to another branch

--- bin/lambda.ts
+++ bin/lambda.ts
@@ -2,7 +2,6 @@
 import "source-map-support/register"
 import cdk = require("aws-cdk-lib")
 import { CDKExampleLambdaApiStack } from "../lib/lambda-bedrock-stack"
-import { AmplifyInfraStack } from "../lib/amplify-stack"
 
 export const lambdaFunctionName = "ImageDescriptionFunction"
 
@@ -10,5 +9,3 @@ const app = new cdk.App()
 new CDKExampleLambdaApiStack(app, "CDKImageDescriptionStack", {
     functionName: lambdaFunctionName,
 })
-
-new AmplifyInfraStack(app, "CDKFrontEndStack", {})
